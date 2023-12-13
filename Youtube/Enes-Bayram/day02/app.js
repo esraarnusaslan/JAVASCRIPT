af21c9cf7560a62cfd31e068e920fad818dc6eb8
Esra Arnus Aslan <esra.arnus@gmail.com>
2023-12-13 10:19:25 +0300

    "toString, parseInt, parseFloat"

---
+++ Youtube/Enes-Bayram/day02/app.js
@@ -0,0 +1,58 @@
+
+//! String ten number veri tipine donusturmek 1.yol
+/* let a=5;
+let b=Number("10");
+
+console.log(a+b); */
+
+
+//! String ten number veri tipine donusturmek  2.yol
+/* let c=Number(b);
+console.log(typeof c);
+console.log(a+c); */
+
+// ! String ten number veri tipine donusturmek  3.yol
+
+/* let a=5;
+let b=parseInt("10.5");
+
+console.log(a+b); 
+
+parseInt virgullu sayilari tam sayi olarak gosterir. */
+
+
+
+//! String ten number veri tipine donusturmek  4.yol
+
+/* let a=5;
+let b=parseFloat("10.5");
+
+console.log(a+b);
+
+parseFloat virgullu sayilari virgulu ile gosterir. */
+
+
+
+//! Number veri tipinden String veri tipine donusturmek 1.yol
+
+/* let a=String(5);
+console.log(typeof a);
+console.log(a); */
+
+//! Number veri tipinden String veri tipine donusturmek 2.yol
+
+/* let a=(5).toString();
+console.log(typeof a);
+console.log(a);
+
+
+let b=String(true);
+console.log(typeof b);
+console.log(b);
+ 
+let rakamlar=String([1,2,3,4]);
+console.log(typeof rakamlar);
+console.log(rakamlar);
+
+let rakamlar=Number([1,2,3,4]);
+console.log(rakamlar);*/
