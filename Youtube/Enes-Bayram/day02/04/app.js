0c86266507c887f9681e24a10adbd5c556f00de2
Esra Arnus Aslan <esra.arnus@gmail.com>
2023-12-13 10:37:14 +0300

    "console ,alert, document"

---
+++ Youtube/Enes-Bayram/day02/04/app.js
@@ -0,0 +1,16 @@
+// ! Document write ile cikti verme
+
+/* document.write("Hello World! <br/>");
+document.write("Hello World!"); */
+
+
+// ! console log ile cikti verme
+
+/* console.log("Hello World!"); */
+
+
+// ! alert ile cikti verme
+
+console.log(window);
+alert("Hello World!");
+
