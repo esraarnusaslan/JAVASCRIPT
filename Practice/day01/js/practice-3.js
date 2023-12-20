ea743636acf60ed2034e9916386232811a9a615a
Esra Arnus Aslan <esra.arnus@gmail.com>
2023-12-20 14:44:36 +0300

    "practice-3 day01"

---
+++ Practice/day01/js/practice-3.js
@@ -0,0 +1,9 @@
+let kutu1 = document.querySelector('.kutu1');
+let kutu2 = document.querySelector('.kutu2');
+let kutu3 = document.querySelector('.kutu3');
+let kutu4 = document.querySelector('.kutu4');
+
+kutu1.innerHTML = `<h1 style="text-align:center; color:red">Aqua</h1>`;
+kutu2.innerHTML = `<h1 style="text-align:center; color:red">Yellow</h1>`;
+kutu3.innerHTML = `<h1 style="text-align:center; color:red">Wheat</h1>`;
+kutu4.innerHTML = `<h1 style="text-align:center; color:red">Cadetblue</h1>`;
