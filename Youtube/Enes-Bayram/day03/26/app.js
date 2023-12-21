1fa3d0b46c7d05c4d25f567071bc884b4bf30ba3
Esra Arnus Aslan <esra.arnus@gmail.com>
2023-12-21 17:17:09 +0300

    "switch"

---
+++ Youtube/Enes-Bayram/day03/26/app.js
@@ -0,0 +1,25 @@
+let number = prompt('please enter a number between 1 and 5');
+
+switch (number) {
+    case '1':
+        console.log('the entered number is 1.');
+        break;
+
+    case '2':
+        console.log('the entered number is 2.');
+        break;
+
+    case '3':
+        console.log('the entered number is 3.');
+        break;
+
+    case '4':
+        console.log('the entered number is 4.');
+        break;
+
+    case '5':
+        console.log('the entered number is 5.');
+        break;
+    default:
+        console.log('please enter a number between 1 and 5');
+}
