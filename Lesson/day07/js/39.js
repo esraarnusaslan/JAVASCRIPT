ef93ba2d8e950d4432e2114c0aae6daa837527d4
Esra Arnus Aslan <esra.arnus@gmail.com>
2023-12-23 09:39:41 +0300

    "iife"

---
+++ Lesson/day07/js/39.js
@@ -0,0 +1,15 @@
+const sum = function () {
+    alert('hello');
+};
+sum();
+
+//asagidaki ve yukaridaki ayni fonks.
+
+(function () {
+    alert('hello');
+})();
+
+/*  
+! buyuk projelerde birden fazla js dosyasi varsa 
+! hata vermesin diye kullanilir.
+ */
