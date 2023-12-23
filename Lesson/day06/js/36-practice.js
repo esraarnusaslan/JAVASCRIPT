b2da44956072d9cf30f353aa8b16e66db2a958bb
Esra Arnus Aslan <esra.arnus@gmail.com>
2023-12-23 08:57:08 +0300

    "practice"

--- Lesson/day06/js/36-practice.js
+++ Lesson/day06/js/36-practice.js
@@ -13,12 +13,20 @@ const getLastPrice = (price, tax, discount) => {
         discount += 10;
     }
 
-    const totalTax = (price * tax) / 100;
-    const totalDiscount = (price * discount) / 100;
+    const totalTax = price * (tax / 100);
+    const totalDiscount = price * (discount / 100);
     const result = price + totalTax - totalDiscount;
     return result;
 };
 
-const onGetLstPriceClick = () => {
-    
+const onGetLastPriceClick = () => {
+    const txtNum = document.getElementById('txtNum');
+    let num = Number(txtNum.value);
+    const txtTax = document.getElementById('txtTax');
+    let tax = Number(txtTax.value);
+    const txtDiscount = document.getElementById('txtDiscount');
+    let discount = Number(txtDiscount.value);
+
+    const result = getLastPrice(num, tax, discount);
+    alert(result);
 };
