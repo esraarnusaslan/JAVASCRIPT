94a319b474b88461b4153d67f838a1b1b7cda51c
Esra Arnus Aslan <esra.arnus@gmail.com>
2023-12-24 20:56:48 +0300

    "array practice"

---
+++ Lesson/day08/js/47.js
@@ -0,0 +1,15 @@
+const arr = [12, 3, 56, 789, 32, 3];
+
+const setPrices = (arr, rate) => {
+    if (!Array.isArray(arr) || arr.length <= 0) {
+        return;
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = arr[i] + (arr[i] * rate) / 100;
+    }
+    return arr;
+};
+
+const newArr = setPrices(arr, 20);
+console.log('new arr', newArr);
