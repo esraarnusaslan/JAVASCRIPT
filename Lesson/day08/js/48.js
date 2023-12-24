0525d6aa42789dc2ae09da87ae499c027c1e044f
Esra Arnus Aslan <esra.arnus@gmail.com>
2023-12-24 21:29:25 +0300

    "mutator"

---
+++ Lesson/day08/js/48.js
@@ -0,0 +1,26 @@
+const cars = ['Porsche', 'Ferrari', 'Mustang', 'Tesla'];
+console.log(cars);
+let arrLength = cars.push('Lamborghini');
+console.log(cars, arrLength); // ! en sona lamborghini yi ekledi
+
+let deletedItem = cars.pop();
+console.log(cars, deletedItem); // ! son elemani siler. Silinen elemani doner.
+
+deletedItem = cars.shift();
+console.log(cars, deletedItem); // ! ilk elemani siler. Silinen elemani doner.
+
+arrLength = cars.unshift('Lamborghini');
+console.log(cars, arrLength); // ! push ile ayni sadece bu elemani en basa koyar.
+
+cars.sort(); // ? mutator method oldugu icin mevcut diziyi saklar
+console.log(cars); // ! alfabetik siralar.
+
+const newArr = cars.toSorted(); //mutator olmadigi icin mevcut diziyi bozmaz yeni bir dizi olusturup siralamayi onda olusturur.
+console.log(newArr);
+
+cars.reverse();
+console.log(cars); // ! diziyi tersten siralar.
+
+console.log(cars.splice(0, 3));
+
+console.log(cars.slice(0, 2));
