e2ac4bf3cf61a3464f6c0d3832765b5cb0fe44bb
Esra Arnus Aslan <esra.arnus@gmail.com>
2023-12-25 13:07:12 +0300

    "asabeneh day04  ternary practice"

---
+++ Asabeneh-Github/day04/js/03.js
@@ -0,0 +1,6 @@
+let a = Number(prompt('Please enter a number'));
+let b = Number(prompt('Please enter a number'));
+
+a > b
+    ? console.log(`${a} is greater than ${b}`)
+    : console.log(`${b} is greater than ${a}`);
