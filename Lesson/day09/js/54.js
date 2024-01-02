40f378e5fa06ca1b5aa673976a01b0e1ba9395fd
Esra Arnus Aslan <esra.arnus@gmail.com>
2024-01-02 16:04:30 +0300

    "for each"

---
+++ Lesson/day09/js/54.js
@@ -0,0 +1,16 @@
+const names = ['john', 'angela', 'albert', 'mark', 'melissa'];
+const lastName = ['doe', 'al', 'messy', 'jackson', 'jack'];
+
+//! FOR EACH
+/* const cb = (animal) => {
+    console.log(animal);
+};
+names.forEach(cb); */
+
+//! FOR EACH SHORT WAY
+const cb = (animal) => {
+    console.log(animal);
+};
+names.forEach((names) => {
+    console.log(names);
+});
