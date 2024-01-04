514c1687a341e3900ba0f4744c85404af8634f54
Esra Arnus Aslan <esra.arnus@gmail.com>
2024-01-04 14:30:36 +0300

    "primitive-reference"

---
+++ Youtube/Enes-Bayram/day08/js/62.js
@@ -0,0 +1,26 @@
+
+
+//! Primitive
+
+/* 
+String
+Number
+Boolean
+Undefined
+Null
+Symbol
+ */
+
+//! Reference
+
+/* 
+Object
+Array
+Function 
+*/
+
+
+let a=5;
+let b=a;
+console.log(a);
+console.log(b);
