c120c8e1d8924f8b2063e772232d728fec94cf3b
Esra Arnus Aslan <esra.arnus@gmail.com>
2024-01-04 08:09:16 +0300

    "lesson day10 iteration filter practice"

---
+++ Lesson/day10/js/59.js
@@ -0,0 +1,43 @@
+const products = [
+    {
+        id: 1,
+        name: 'laptop',
+        price: 3000,
+    },
+    {
+        id: 2,
+        name: 'phone',
+        price: 5000,
+    },
+
+    {
+        id: 3,
+        name: 'mobile',
+        price: 700,
+    },
+
+    {
+        id: 4,
+        name: 'watch',
+        price: 500,
+    },
+    {
+        id: 5,
+        name: 'mouse',
+        price: 600,
+    },
+    {
+        id: 6,
+        name: 'monitor',
+        price: 500,
+    },
+];
+
+const filterProducts = (c) => {
+    const filteredProducts = products.filter((product) =>
+        product.name.startsWith(c)
+    );
+    return filteredProducts;
+};
+
+console.log(filterProducts('m'));
