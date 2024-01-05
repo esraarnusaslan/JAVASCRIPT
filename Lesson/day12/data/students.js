6e5ca7f8296df5ae9ea28e5ca54150872ceffde1
Esra Arnus Aslan <esra.arnus@gmail.com>
2024-01-05 21:52:06 +0300

    "dom-practice"

---
+++ Lesson/day12/data/students.js
@@ -0,0 +1,25 @@
+export const students = [
+    {
+        id: 1,
+        name: 'John',
+        point: 80,
+    },
+
+    {
+        id: 2,
+        name: 'Mary',
+        point: 32,
+    },
+
+    {
+        id: 3,
+        name: 'Maggie',
+        point: 56,
+    },
+
+    {
+        id: 4,
+        name: 'Tom',
+        point: 76,
+    },
+];
