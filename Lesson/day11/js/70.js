f42189496fdc12245c27879d035c38b89ba76143
Esra Arnus Aslan <esra.arnus@gmail.com>
2024-01-05 16:36:03 +0300

    "dom textContent"

---
+++ Lesson/day11/js/70.js
@@ -0,0 +1,3 @@
+const message = document.getElementById('message');
+console.log(message.innerText); //JavaScript DOM manipulation is funny
+console.log(message.textContent); //JavaScript DOM manipulation is VERY funny. bosluklari da alir.
