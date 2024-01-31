47eb3847486abc8ccf85f50896a807228b3dd119
Esra Arnus Aslan <esra.arnus@gmail.com>
2024-01-31 17:55:48 +0300

    "fetch and axios practice"

--- Lesson/day17/js/105.js
+++ Lesson/day17/js/105.js
@@ -1,83 +1,79 @@
-const baseUrl = "https://651da7e644e393af2d5a2461.mockapi.io/api/v1";
+const baseUrl = 'https://65b8db22b71048505a899dd1.mockapi.io/api/v1';
 
-  const getUsers = async () => { 
-      try {
-          const resp = await fetch(`${baseUrl}/users`)
-          const data = await resp.json();
-          return data.reverse();
-      
-      } catch (err) {
-          console.log(err);
-          return [];
-      }
-  }
-  
-  
-  const createUser = async(user) => { 
-      if(!user.firstName) throw new Error("Enter your first name")
-      if(!user.lastName) throw new Error("Enter your last name")
-      
-      const resp = await fetch(`${baseUrl}/users`,{
-        method: "post",
+const getUsers = async () => {
+    try {
+        const resp = await fetch(`${baseUrl}/users`);
+        const data = await resp.json();
+        return data.reverse();
+    } catch (err) {
+        console.log(err);
+        return [];
+    }
+};
+
+const createUser = async (user) => {
+    if (!user.firstName) throw new Error('Enter your first name');
+    if (!user.lastName) throw new Error('Enter your last name');
+
+    const resp = await fetch(`${baseUrl}/users`, {
+        method: 'post',
         body: JSON.stringify(user), // javascript object i json string e çevirir
-        headers:{
-          "Content-Type": "application/json",
-          "Accept": "application/json"
-        }
-      })
-  
-      const data = await resp.json();
-      return data;
-  }
-  
-  const deleteUser = async(id) => { 
-    if(!id) throw new Error("Id is required")
-    
-    const resp = await fetch(`${baseUrl}/users/${id}`,{
-      method: "delete",
-    })
-  
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+        },
+    });
+
     const data = await resp.json();
     return data;
-  }
-  
-  
-  document.getElementById("btnAdd").addEventListener("click", async ()=>{
-      try {
-        const txtFirstName = document.getElementById("txtFirstName");
-        const txtLastName = document.getElementById("txtLastName");
-  
+};
+
+const deleteUser = async (id) => {
+    if (!id) throw new Error('Id is required');
+
+    const resp = await fetch(`${baseUrl}/users/${id}`, {
+        method: 'delete',
+    });
+
+    const data = await resp.json();
+    return data;
+};
+
+document.getElementById('btnAdd').addEventListener('click', async () => {
+    try {
+        const txtFirstName = document.getElementById('txtFirstName');
+        const txtLastName = document.getElementById('txtLastName');
+
         const user = {
-          firstName: txtFirstName.value,
-          lastName: txtLastName.value
-        }
-  
+            firstName: txtFirstName.value,
+            lastName: txtLastName.value,
+        };
+
         const newUser = await createUser(user);
         const strHTML = createUserCard(newUser);
-  
-        document.getElementById("usersEl").insertAdjacentHTML("afterbegin",strHTML);
-        
-      } catch (err) {
-        console.log(err)
+
+        document
+            .getElementById('usersEl')
+            .insertAdjacentHTML('afterbegin', strHTML);
+    } catch (err) {
+        console.log(err);
         alert(err.message);
-      }
-  })
-  
-  
-  const loadUserData =  async () => { 
-      const users  = await getUsers();
-      let strHTML = "";
-  
-      users.forEach( (user)=>{
-          strHTML += createUserCard(user)
-      })
-  
-      document.getElementById("usersEl").innerHTML = strHTML;
-      createDeleteEvents();
-  }
-  
-  
-  const createUserCard = (user) => { 
+    }
+});
+
+const loadUserData = async () => {
+    const users = await getUsers();
+    let strHTML = '';
+
+    users.forEach((user) => {
+        strHTML += createUserCard(user);
+    });
+
+    document.getElementById('usersEl').innerHTML = strHTML;
+    createDeleteEvents();
+};
+
+const createUserCard = (user) => {
     const { avatar, firstName, lastName, id } = user;
     return `<div class="col-6 col-md-3 user" data-id="${id}">
           <button class="btn btn-light btn-delete">❌</button>
@@ -87,29 +83,21 @@ const baseUrl = "https://651da7e644e393af2d5a2461.mockapi.io/api/v1";
               <h5 class="card-title">${firstName} ${lastName}</h5>
             </div>
           </div>
-        </div>`
-  
-  }
+        </div>`;
+};
 
-  const createDeleteEvents = () => { 
-    
-    document.querySelectorAll(".btn-delete").forEach( (item)=>{
-      item.addEventListener("click", (e)=>{
+const createDeleteEvents = () => {
+    document.querySelectorAll('.btn-delete').forEach((item) => {
+        item.addEventListener('click', (e) => {
+            const result = confirm('Are you sure to delete?');
+            if (!result) return;
 
-        const result = confirm("Are you sure to delete?");
-        if(!result) return;
+            const userEl = item.closest('.user');
+            const id = userEl.getAttribute('data-id');
+            deleteUser(id);
+            userEl.remove();
+        });
+    });
+};
 
-
-        const userEl = item.closest(".user");
-        const id = userEl.getAttribute("data-id");
-        deleteUser(id);
-        userEl.remove();
-      });
-    })
-  }
-  
-  
-  
-  
-   loadUserData();
-   
+loadUserData();
