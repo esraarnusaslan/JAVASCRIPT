73a4091461c43acc830342ab57d424ec93d9fc9e
Esra Arnus Aslan <esra.arnus@gmail.com>
2024-02-15 19:04:20 +0300

    "super()"

--- Youtube/Enes-Bayram/day11/js/97.js
+++ Youtube/Enes-Bayram/day11/js/97.js
@@ -19,3 +19,20 @@ math.add(100, 200);
 
 //! this is static
 Mathematics.subtract(10, 3);
+
+class Person {
+    firstName = 'Esra';
+
+    write() {
+        console.log(this.firstName);
+    }
+}
+
+class Student extends Person {
+    write() {
+        super.write();
+    }
+}
+
+const student1 = new Student();
+student1.write();
