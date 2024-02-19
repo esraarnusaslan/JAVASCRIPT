a3ac51c37f210a696e10b14cbce80613aeabef9f
Esra Arnus Aslan <esra.arnus@gmail.com>
2024-02-19 16:04:31 +0300

    "fetch api"

---
+++ Youtube/Enes-Bayram/day13/js/109.js
@@ -0,0 +1,20 @@
+/* const getStudents = (url) => {
+    fetch(url) //Promise is Response type
+        .then((response) => {
+            return response.json(); //return Promise
+        })
+        .then((data) => console.log(data))
+        .catch((err) => console(err));
+};
+
+getStudents('../students.json'); */
+
+const getData = (url) => {
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => console.log(data))
+        .catch((err) => console.log(err));
+};
+
+getData('https://jsonplaceholder.typicode.com/users');
+getData('https://jsonplaceholder.typicode.com/albums');
