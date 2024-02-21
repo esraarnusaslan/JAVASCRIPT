12204e12c73061a23a39cb6e72e01f8f3fbee9e0
Esra Arnus Aslan <esra.arnus@gmail.com>
2024-02-21 13:10:17 +0300

    "js practice 03"

--- Practice/day03/js/03.js
+++ Practice/day03/js/03.js
@@ -3,5 +3,20 @@ let price = document.getElementById('price');
 let message = document.getElementById('message');
 
 document.getElementById('calculate').addEventListener('click', () => {
-    
+    let productValue = product.value;
+    let priceValue = Number(price.value);
+
+    let counter = 1;
+    let sum = 0;
+
+    do {
+        sum = counter * priceValue;
+        if (sum > 1500) {
+            sum -= priceValue;
+            counter--;
+            break;
+        }
+        counter++;
+    } while (sum <= 1500);
+    message.innerText=`Number of products : ${counter} Sum of products : ${sum}`
 });
