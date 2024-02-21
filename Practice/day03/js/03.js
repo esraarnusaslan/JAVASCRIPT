f7b726d3b4185784d8d75587841fb24f9160a943
Esra Arnus Aslan <esra.arnus@gmail.com>
2024-02-21 12:58:35 +0300

    "js practice 03"

---
+++ Practice/day03/js/03.js
@@ -0,0 +1,7 @@
+let product = document.getElementById('product');
+let price = document.getElementById('price');
+let message = document.getElementById('message');
+
+document.getElementById('calculate').addEventListener('click', () => {
+    
+});
